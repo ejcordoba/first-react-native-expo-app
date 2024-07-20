926cdbf3248942e7eed9b636f6aec089e3249d5b
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-07-20 11:27:43 +0200

    Layout, card and animations

---
+++ react-native-metacritic-app/components/main.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { FlatList, View, ScrollView, ActivityIndicator } from "react-native";
+import { getLatestGames } from "../lib/metacritic.js";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { AnimatedGameCard, GameCard } from "./GameCard.jsx";
+import { Logo } from "./Logo.jsx";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+  return (
+    <View style={{ paddingTop: insets.top + 20, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
+      {games.length === 0 ? (
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+      )}
+    </View>
+  );
+}
